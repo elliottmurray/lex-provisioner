1e6c19988b48f07effaab8f58346c574a3030234
Elliott Murray <elliottmurray@gmail.com>
2019-01-08 21:54:16 +1100

    init commit

---
+++ hello_world/app.py
@@ -0,0 +1,171 @@
+import json
+
+import requests
+
+
+import os
+import json
+import crhelper
+from put import LexBotBuilder
+
+# initialise logger
+logger = crhelper.log_config({"RequestId": "CONTAINER_INIT"})
+logger.info('Logging configured')
+# set global to track init failures
+init_failed = False
+
+try:
+    # Place initialization code here
+    logger.info("Container initialization completed")
+except Exception as ex:
+    logger.error(ex, exc_info=True)
+    init_failed = ex
+
+
+LEX_DEFINITION_FILENAME = 'lex-definition.json'
+def _read_lex_definition_file(file_name=LEX_DEFINITION_FILENAME):
+    """Reads lex definition file (json) which is packaged in the same directory"""
+    with open(file_name) as lex_json_file:
+        lex_definition = json.load(lex_json_file)
+    logger.info('successfully read bot definition from file: %s', file_name)
+    return lex_definition
+
+def _get_function_arn(function_name, aws_region, aws_account_id, prefix):
+    return 'arn:aws:lambda:' + aws_region + ':' + aws_account_id \
+        + ':function:' + prefix + function_name
+
+def _add_prefix(lex_definition, name_prefix, aws_region, aws_account_id):
+    """Add _name_prefix to all resource names in a lex-definition
+
+    This will help differentiate lex-resources when running multiple stacks
+    in the same AWS account & region
+    """
+    bot = lex_definition['bot']
+    bot['name'] = name_prefix + bot['name']
+
+    bot['intents'] = list(map(
+        lambda intent: dict(
+            intentName=(name_prefix + intent.pop('intentName')),
+            **intent
+        ),
+        lex_definition['bot']['intents']
+    ))
+
+    slot_types = list(
+        map(
+            lambda slot_type: dict(
+                name=(name_prefix + slot_type.pop('name')),
+                **slot_type
+            ),
+            lex_definition['slot_types']
+        )
+    )
+
+    intents = list(
+        map(
+            lambda intent: dict(
+                name=(name_prefix + intent.pop('name')),
+                slots=list(
+                    map(
+                        lambda slot: dict(
+                            slotType=(name_prefix + slot.pop('slotType')),
+                            **slot
+                        ) if not slot['slotType'].startswith('AMAZON.') else slot,
+                        intent.pop('slots')
+                    )
+                ),
+                **intent
+            ),
+            lex_definition['intents']
+        )
+    )
+
+    for intent in intents:
+        if 'dialogCodeHook' in intent:
+            intent['dialogCodeHook']['uri'] = _get_function_arn(
+                intent['dialogCodeHook']['uri'], aws_region, aws_account_id, name_prefix
+            )    
+        intent['fulfillmentActivity']['codeHook']['uri'] = _get_function_arn(
+            intent['fulfillmentActivity']['codeHook']['uri'], aws_region, aws_account_id, name_prefix
+        )
+
+    return dict(
+        bot=bot,
+        intents=intents,
+        slot_types=slot_types
+    )
+
+def _lex_builder_instance(event, context):
+    """Creates an instance of LexBotBuilder"""
+    lex_definition = _read_lex_definition_file()
+    resource_properties = event.get('ResourceProperties')
+
+    name_prefix = resource_properties.get('NamePrefix')
+    logger.info('name prefix is: %s', name_prefix)
+    aws_account_id = context.invoked_function_arn.split(':')[4]
+    aws_region = os.environ['AWS_REGION']
+
+    lex_definition_with_prefix = _add_prefix(lex_definition, name_prefix, aws_region, aws_account_id)
+    logger.info('lex definition with prefix: %s', lex_definition_with_prefix)
+
+    return lex_definition_with_prefix, LexBotBuilder(logger)
+
+def create(event, context):
+    """
+    Handle Create events
+
+    To return a failure to CloudFormation simply raise an exception,
+    the exception message will be sent to CloudFormation Events.
+    """
+    lex_definition, lex_bot_builder = _lex_builder_instance(event, context)
+
+    bot_put_response = lex_bot_builder.put(lex_definition)
+    response_data = dict(
+        BotName=lex_definition['bot']['name'],
+        BotVersion=bot_put_response['version']
+    )
+
+    return response_data
+
+
+def update(event, context):
+    """
+    Handle Update events
+
+    To return a failure to CloudFormation simply raise an exception,
+    the exception message will be sent to CloudFormation Events.
+    """
+    lex_definition, lex_bot_builder = _lex_builder_instance(event, context)
+
+    bot_put_response = lex_bot_builder.put(lex_definition)
+    response_data = dict(
+        BotName=lex_definition['bot']['name'],
+        BotVersion=bot_put_response['version']
+    )
+
+    return response_data
+
+
+def delete(event, context):
+    """
+    Handle Delete events
+
+    To return a failure to CloudFormation simply raise an exception,
+    the exception message will be sent to CloudFormation Events.
+    """
+    lex_definition, lex_bot_builder = _lex_builder_instance(event, context)
+    lex_bot_builder.delete(lex_definition)
+    return
+
+
+def lambda_handler(event, context):
+    """
+    Main handler function, passes off it's work to crhelper's cfn_handler
+    """
+    # update the logger with event info
+    global logger
+    
+    logger = crhelper.log_config(event)
+    logger.info('event: %s', json.dumps(event, indent=4, sort_keys=True, default=str))
+    return crhelper.cfn_handler(event, context, create, update, delete, logger,
+                                init_failed)
---
+++ hello_world/put.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python
+
+""" Provision AWS Lex resources using python SDK
+"""
+
+import time
+import boto3
+from botocore.exceptions import ClientError
+
+class LexBotBuilder:
+    """Create/Update different elements that make up a Lex bot"""
+    def __init__(self, logger):
+        self._logger = logger
+        self._lex_sdk = self._get_lex_sdk()
+
+    def _get_lex_sdk(self):
+        return boto3.Session().client('lex-models')
+
+    def _get_lambda_sdk(self):
+        return boto3.Session().client('lambda')
+
+    def _create_lex_resource(self, func, func_name, properties):
+        try:
+            response = func(**properties)
+            self._logger.info('Created lex resource using %s, response: %s', func_name, response)
+            return response
+        except Exception as ex:
+            self._logger.error('Failed to create lex resource using %s', func_name)
+            self._logger.error(ex)
+            raise
+
+    def _update_lex_resource(self, func, func_name, checksum, properties):
+        try:
+            response = func(checksum=checksum, **properties)
+            self._logger.info('Created lex resource using %s, response: %s', func_name, response)
+            return response
+        except Exception as ex:
+            self._logger.error('Failed to update lex resource using %s', func_name)
+            self._logger.error(ex)
+            raise
+
+    def _replace_intent_version(self, bot_definition, intents):
+        for intent in bot_definition['intents']:
+            intent['intentVersion'] = intents[intent['intentName']]
+        return bot_definition
+
+    def _put_bot(self, bot_properties):
+        """Create/Update bot"""
+        bot_name = bot_properties['name']
+        try:
+            get_bot_response = self._lex_sdk.get_bot(
+                name=bot_name, versionOrAlias='$LATEST')
+            checksum = get_bot_response['checksum']
+        except ClientError as ex:
+            http_status_code = None
+            if 'ResponseMetadata' in ex.response:
+                response_metadata = ex.response['ResponseMetadata']
+                if 'HTTPStatusCode' in response_metadata:
+                    http_status_code = response_metadata['HTTPStatusCode']
+            if http_status_code == 404:
+                creation_response = self._create_lex_resource(self._lex_sdk.put_bot, 'put_bot', bot_properties)
+                version_response = self._lex_sdk.create_bot_version(name=bot_name, checksum=creation_response['checksum'])
+                return version_response
+            else:
+                self._logger.info('Lex get_bot call failed')
+                self._logger.info(ex)
+                raise
+
+        update_response = self._update_lex_resource(self._lex_sdk.put_bot, 'put_bot', checksum, bot_properties)
+        version_response = self._lex_sdk.create_bot_version(name=bot_name, checksum=update_response['checksum'])
+        return version_response
+
+    def _get_intent_arn(self, intent_name, aws_region, aws_account_id):
+        return 'arn:aws:lex:' + aws_region + ':' + aws_account_id \
+            + ':intent:' + intent_name + ':*'
+
+    def _create_intent(self, intent_definition):
+        """Create intent and configure any required lambda permissions
+
+        Currently only supports intents that use the same lambda for both
+        code hooks (i.e. 'dialogCodeHook' and 'fulfillmentActivity')
+        """
+        code_hook = None
+        if 'dialogCodeHook' in intent_definition:
+            code_hook = intent_definition['dialogCodeHook']
+        elif 'fulfillmentActivity' in intent_definition:
+            if 'codeHook' in intent_definition['fulfillmentActivity']:
+                code_hook = intent_definition['fulfillmentActivity']['codeHook']
+        
+        # TODO if the intent does not need to invoke a lambda, create it
+        if code_hook:
+            # If the intent needs to invoke a lambda function, we must give it permission to do so
+            # before creating the intent.
+            arn_tokens = code_hook['uri'].split(':')
+            aws_region = arn_tokens[3]
+            aws_account_id = arn_tokens[4]
+            lambda_sdk = self._get_lambda_sdk()
+            statement_id = 'lex-' + aws_region + '-' + intent_definition['name']
+            try:
+                add_permission_response = lambda_sdk.add_permission(
+                    FunctionName=code_hook['uri'],
+                    StatementId=statement_id,
+                    Action='lambda:InvokeFunction',
+                    Principal='lex.amazonaws.com',
+                    SourceArn=self._get_intent_arn(intent_definition['name'], aws_region, aws_account_id)
+                )
+                self._logger.info(
+                    'Response for adding intent permission to lambda: %s', add_permission_response
+                )
+            except ClientError as ex:
+                if ex.response['Error']['Code'] == 'ResourceConflictException':
+                    self._logger.info('Failed to add permission to lambda, it already exists')
+                    self._logger.info(ex)
+                else:
+                    raise
+
+        new_intent = self._create_lex_resource(
+            self._lex_sdk.put_intent, 'put_intent', intent_definition
+        )
+        self._logger.info('Created new intent: %s', new_intent)
+        return new_intent
+
+    def _replace_slot_type_version(self, intents_definition, slot_types):
+        for intent in intents_definition:
+            for slot in intent['slots']:
+                if not slot['slotType'].startswith('AMAZON.'):
+                    slot['slotTypeVersion'] = slot_types[slot['slotType']]
+        return intents_definition
+
+    def _put_intents(self, intents_definition):
+        """Create/Update intents and return the new version created for each
+
+        Arguments:
+        intents_definition -- array of intent objects containing all properties required by the aws-lex-sdk's put_intent function
+        slot_type_versions -- dict that maps slot_type name -> version
+        """
+        intent_versions = {}
+        for intent in intents_definition:
+            name = intent['name']
+            lookup_version = '$LATEST'
+            try:
+                get_intent_response = self._lex_sdk.get_intent(name=name, version=lookup_version)
+                checksum = get_intent_response['checksum']
+            except ClientError as ex:
+                http_status_code = None
+                if 'ResponseMetadata' in ex.response:
+                    response_metadata = ex.response['ResponseMetadata']
+                    if 'HTTPStatusCode' in response_metadata:
+                        http_status_code = response_metadata['HTTPStatusCode']
+                if http_status_code == 404:
+                    creation_response = self._create_intent(intent)
+                    version_response = self._lex_sdk.create_intent_version(name=name, checksum=creation_response['checksum'])
+                    intent_versions[name] = version_response['version']
+                    continue
+                else:
+                    self._logger.info('Lex get_slot_type call failed')
+                    self._logger.info(ex)
+                    raise
+
+            update_response = self._update_lex_resource(self._lex_sdk.put_intent, 'put_intent', checksum, intent)
+            version_response = self._lex_sdk.create_intent_version(name=name, checksum=update_response['checksum'])
+            intent_versions[name] = version_response['version']
+        return intent_versions
+
+    def _put_slot_types(self, slot_type_definition):
+        """Create/Update slot_types"""
+        slot_type_versions = {}
+        for slot_type in slot_type_definition:
+            name = slot_type['name']
+            lookup_version = '$LATEST'
+            try:
+                get_slot_type_response = self._lex_sdk.get_slot_type(name=name, version=lookup_version)
+                checksum = get_slot_type_response['checksum']
+            except ClientError as ex:
+                http_status_code = None
+                if 'ResponseMetadata' in ex.response:
+                    response_metadata = ex.response['ResponseMetadata']
+                    if 'HTTPStatusCode' in response_metadata:
+                        http_status_code = response_metadata['HTTPStatusCode']
+                if http_status_code == 404:
+                    creation_response = self._create_lex_resource(
+                        self._lex_sdk.put_slot_type, 'put_slot_type', slot_type)
+                    version_response = self._lex_sdk.create_slot_type_version(name=name, checksum=creation_response['checksum'])
+                    slot_type_versions[name] = version_response['version']
+                    continue
+                else:
+                    self._logger.info('Lex get_slot_type call failed')
+                    self._logger.info(ex)
+                    raise
+
+            update_response = self._update_lex_resource(self._lex_sdk.put_slot_type, 'put_slot_type', checksum, slot_type)
+            version_response = self._lex_sdk.create_slot_type_version(name=name, checksum=update_response['checksum'])
+            slot_type_versions[name] = version_response['version']
+        return slot_type_versions
+
+    def put(self, lex_definition):
+        """Create/Update lex-bot resources; bot, intents, slot_types"""
+        slot_type_versions = self._put_slot_types(lex_definition['slot_types'])
+
+        intents_definition = self._replace_slot_type_version(lex_definition['intents'], slot_type_versions)
+        intent_versions = self._put_intents(intents_definition)
+
+        bot_definition = self._replace_intent_version(lex_definition['bot'], intent_versions)
+        bot_response = self._put_bot(lex_definition['bot'])
+        return bot_response
+
+
+    MAX_DELETE_TRIES = 5
+    RETRY_SLEEP = 5
+    def _delete_bot(self, bot_definition):
+        '''Delete bot'''
+        bot_name = bot_definition['name']
+        self._logger.info('deleting bot: %s', bot_name)
+        count = self.MAX_DELETE_TRIES
+        while True:
+            try:
+                self._lex_sdk.delete_bot(name=bot_name)
+                self._logger.info('deleted bot: %s', bot_name)
+                break
+            except Exception as ex:
+                self._logger.warning('Lex delete_bot call failed')
+                self._logger.warning(ex)
+                count -= 1
+                if count:
+                    self._logger.warning(
+                        'Lex delete_bot retry: %s. Sleeping for %s seconds',
+                        self.MAX_DELETE_TRIES - count,
+                        self.RETRY_SLEEP
+                    )
+                    time.sleep(self.RETRY_SLEEP)
+                    continue
+                else:
+                    self._logger.error('Lex delete_bot call max retries')
+                    raise
+
+    def _delete_intents(self, intents_definition):
+        '''Delete all intent'''
+        for intent in intents_definition:
+            name = intent['name']
+            self._logger.info('deleting intent: %s', name)
+            count = self.MAX_DELETE_TRIES
+            while True:
+                try:
+                    self._lex_sdk.delete_intent(name=name)
+                    self._logger.info('successfully deleted intent: %s', name)
+                    break
+                except Exception as ex:
+                    self._logger.warning('Lex delete_intent call failed')
+                    self._logger.warning(ex)
+                    count-=1
+                    if count:
+                        self._logger.warning(
+                            'Lex delete_intent retry: %s. Sleeping for %s seconds',
+                            self.MAX_DELETE_TRIES - count,
+                            self.RETRY_SLEEP
+                        )
+                        time.sleep(self.RETRY_SLEEP)
+                        continue
+                    else:
+                        self._logger.error('Lex delete_intent call max retries')
+                        raise
+    
+    def _delete_slot_types(self, slot_types_definition):
+        '''Delete all slot_type'''
+        for slot_type in slot_types_definition:
+            name = slot_type['name']
+            self._logger.info('deleting slot type: %s', name)
+            count = self.MAX_DELETE_TRIES
+            while True:
+                try:
+                    self._lex_sdk.delete_slot_type(name=name)
+                    self._logger.info('successfully deleted slot type: %s', name)
+                    break
+                except Exception as ex:
+                    self._logger.warning('Lex delete_slot_type call failed')
+                    self._logger.warning(ex)
+                    count-=1
+                    if count:
+                        self._logger.warning(
+                            'Lex delete_slot_type retry: %s. Sleeping for %s seconds',
+                            self.MAX_DELETE_TRIES - count,
+                            self.RETRY_SLEEP
+                        )
+                        time.sleep(self.RETRY_SLEEP)
+                        continue
+                    else:
+                        self._logger.error('Lex delete_slot_type call max retries')
+                        raise
+
+    def delete(self, lex_definition):
+        """Delete bot, intents, and slot-types"""
+        delete_failed = False
+        # TODO what about deleting published version(s) of the bot?
+        try:
+            self._delete_bot(lex_definition['bot'])
+        except Exception as ex:
+            delete_failed = True
+
+        try:
+            self._delete_intents(lex_definition['intents'])
+        except Exception as ex:
+            delete_failed = True
+
+        try:
+            self._delete_slot_types(lex_definition['slot_types'])
+        except Exception as ex:
+            delete_failed = True
+        
+        if delete_failed:
+            raise Exception('See logs for details on what resources failed to delete')
+
+        self._logger.info('Successfully deleted bot and associated resources')
