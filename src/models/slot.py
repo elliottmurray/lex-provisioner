10ffa0625594e24e47a0de6cde1cb0f7afaa9f19
Elliott Murray <elliottmurray@gmail.com>
2019-11-30 18:17:33 +1100

    wip

--- src/models/slot.py
+++ src/models/slot.py
@@ -1,12 +1,12 @@
 class Slot(object):
-
+    """ the slot class """
     def __init__(self, name, slot_type, prompt, utterances):
         self.name = name
-        slot_type == [] if slot_type == None else slot_type
+        slot_type == [] if slot_type is None else slot_type
         self.slot_type = slot_type
         self.prompt = prompt
         self.utterances = utterances
-        
+
     def validate_slot(self):
         if self.utterances is None:
             raise Exception("Utterances missing in slot %s", self.name)
@@ -14,16 +14,16 @@ class Slot(object):
     def __eq__(self, other):
         """Override the default Equals behavior"""
         if isinstance(self, other.__class__):
-            return (self.name == other.name)                
+            return self.name == other.name
         return False
 
     @classmethod
     def create_slots(cls, slot_definitions):
+        """ create slots """
         slots = []
-        if (slot_definitions == None): return slots             
-
+        if (slot_definitions is None): return slots
         for slot_def in slot_definitions:
-            slot = Slot(slot_def.get('Name'), 
+            slot = Slot(slot_def.get('Name'),
                         slot_def.get('Type'),
                         slot_def.get('Prompt'),
                         slot_def.get('Utterances'))
